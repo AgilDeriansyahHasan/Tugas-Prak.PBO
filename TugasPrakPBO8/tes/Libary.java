249f20c5148fe082e3d2bbe854cb78aebd5c9b95
Agil Deriansyah Hasan <agilderiansyahhasan@gmail.com>
2024-11-07 15:22:16 +0700

    Tugas Prak PBO 09

---
+++ TugasPrakPBO8/tes/Libary.java
@@ -0,0 +1,10 @@
+package TugasPrakPBO8.tes;
+
+public class Libary {
+    static{
+        System.out.println("Libary System initialized");
+    }
+    public static String convertToUpperCase(String str){
+        return str.toUpperCase();
+    }
+}
